367307abae1186e246a949a63e1b7aa5fe4e54eb
Ali <vaincoolkid25@gmail.com>
2023-02-26 13:39:59 -0800

    Initial commit

---
+++ src/main/java/com/github/Vain25/BetterCities.java
@@ -0,0 +1,42 @@
+package com.github.Vain25;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(BetterCities.MOD_ID)
+public class BetterCities {
+    public static final String MOD_ID = "better_cities";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public BetterCities() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        StructureRegistry.STRUCTURES.register(modEventBus);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+}
---
+++ src/main/java/com/github/Vain25/StructureRegistry.java
@@ -0,0 +1,21 @@
+package com.github.Vain25;
+
+import com.github.Vain25.structures.UkSub;
+import com.mojang.serialization.Codec;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.StructureType;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class StructureRegistry {
+
+    public static final DeferredRegister<StructureType<?>> STRUCTURES = DeferredRegister.create(Registries.STRUCTURE_TYPE, BetterCities.MOD_ID);
+
+    public static final RegistryObject<StructureType<UkSub>> UK_SUB = STRUCTURES.register("uk_sub", () -> explicitStructureTypeTyping(UkSub.CODEC));
+
+    private static <T extends Structure> StructureType<T> explicitStructureTypeTyping(Codec<T> structureCodec) {
+        return () -> structureCodec;
+    }
+
+}
