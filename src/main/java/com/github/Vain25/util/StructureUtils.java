35eb400d1af85d19b1e7d4a7f7a772e561730f15
X3RMINATION <nicolaeungur@gmail.com>
2023-03-13 18:59:11 -0700

    Added indoors T-T

---
+++ src/main/java/com/github/Vain25/util/StructureUtils.java
@@ -0,0 +1,14 @@
+package com.github.Vain25.util;
+
+public final class StructureUtils {
+
+    private StructureUtils(){}
+
+    public static boolean isChunkAreaFlat(int chunkRadius) {
+        return false;
+    }
+
+    public static boolean isChunkFlat() {
+        return false;
+    }
+}
