b9c17097333e92e517988be04d603ab18ffc703a
X3RMINATION <nicolaeungur@gmail.com>
2023-03-16 22:17:33 -0700

    Updated structures and added biome filtering to generation

--- src/main/java/com/github/Vain25/structures/UkSub.java
+++ src/main/java/com/github/Vain25/structures/UkSub.java
@@ -57,7 +57,6 @@ public class UkSub extends Structure {
     private static boolean extraSpawningChecks(Structure.GenerationContext context) {
         // Grabs the chunk position we are at
         ChunkPos chunkpos = context.chunkPos();
-
         return context.chunkGenerator().getFirstOccupiedHeight(
                 chunkpos.getMinBlockX(),
                 chunkpos.getMinBlockZ(),
@@ -68,7 +67,7 @@ public class UkSub extends Structure {
 
     @Override
     public Optional<Structure.GenerationStub> findGenerationPoint(Structure.GenerationContext context) {
-        if (!StructureUtils.isChunkAreaFlat(context, 64 / 16, 12)) {
+        if (extraSpawningChecks(context) && !StructureUtils.isChunkAreaFlat(context, 64 / 16, 12)) {
             return Optional.empty();
         }
 
